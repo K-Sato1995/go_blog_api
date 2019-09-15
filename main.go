3dfae571ce9707712ecca0eaa58f93ae4b7a316d
Katsuki <Katsuki_Sato0130@icloud.com>
2019-09-15 17:59:26 +0900

    Add get categories request

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-type Client struct {
-	BaseURL    *url.URL
-	UserAgent  string
-	httpClient *http.Client
-}
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const baseURL string = "https://k-blog0130.herokuapp.com/api/v2/"
 
 func main() {
+	GetCategories()
+}
 
+func GetCategories() {
+	url := fmt.Sprintf(baseURL + "categories")
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
 }
 
 // https://medium.com/@marcus.olsson/writing-a-go-client-for-your-restful-api-c193a2f4998c
+// https://www.scaledrone.com/blog/creating-an-api-client-in-go/
